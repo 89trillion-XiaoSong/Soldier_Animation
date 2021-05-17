2ced1bfe92773b714ca4cb65244cb5828460e1cb
89trillion-XiaoSong <xiaosong@89trillion.com>
2021-05-17 15:11:41 +0800

    Initial commit

---
+++ Assets/Script/SoliderController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoliderController : MonoBehaviour
+{
+    [SerializeField] private Animator anim;
+
+    private bool isRun;
+    void Update()
+    {
+        if (Input.GetKeyDown((KeyCode.I)))
+        {
+            anim.SetTrigger("Idle");
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            anim.SetTrigger("Attack");
+        }
+        if (Input.GetKey(KeyCode.R))
+        {
+            isRun = true;
+            anim.SetBool("Run",isRun);
+        }
+        else
+        {
+            isRun = false;
+            anim.SetBool("Run",false);
+        }
+        if(Input.GetKeyDown(KeyCode.W))
+        {
+            anim.SetTrigger("Win");
+        }
+    }
+}

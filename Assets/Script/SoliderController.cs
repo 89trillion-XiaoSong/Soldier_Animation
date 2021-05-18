546fb2c84324ecc825f005017f1ad0af60e83464
89trillion-XiaoSong <xiaosong@89trillion.com>
2021-05-18 15:36:55 +0800

    动画修改，去掉无关类。

--- Assets/Script/SoliderController.cs
+++ Assets/Script/SoliderController.cs
@@ -1,32 +1,26 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class SoliderController : MonoBehaviour
 {
     [SerializeField] private Animator anim;
-
-    private bool isRun;
+    
     void Update()
     {
         if (Input.GetKeyDown((KeyCode.I)))
         {
             anim.SetTrigger("Idle");
         }
+        
         if (Input.GetKeyDown(KeyCode.A))
         {
             anim.SetTrigger("Attack");
         }
+        
         if (Input.GetKey(KeyCode.R))
         {
-            isRun = true;
-            anim.SetBool("Run",isRun);
+            anim.SetTrigger("Run");
         }
-        else
-        {
-            isRun = false;
-            anim.SetBool("Run",false);
-        }
+        
         if(Input.GetKeyDown(KeyCode.W))
         {
             anim.SetTrigger("Win");
